c9b7c73d1be9a155ac44b0d87dbfc2256fb65b70
Pei-Ming Wu <p408865@gmail.com>
2020-07-18 10:43:42 +0800

    refactor: rename main to mizukinana

---
+++ cmd/mizukinana/mizukinana.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/mkfsn/mizukinana/cmd/mizukinana/nanaparty"
+	"github.com/mkfsn/mizukinana/cmd/mizukinana/version"
+)
+
+const (
+	banner = `
+##     ## #### ######## ##     ## ##    ## ####    ##    ##    ###    ##    ##    ###
+###   ###  ##       ##  ##     ## ##   ##   ##     ###   ##   ## ##   ###   ##   ## ##
+#### ####  ##      ##   ##     ## ##  ##    ##     ####  ##  ##   ##  ####  ##  ##   ##
+## ### ##  ##     ##    ##     ## #####     ##     ## ## ## ##     ## ## ## ## ##     ##
+##     ##  ##    ##     ##     ## ##  ##    ##     ##  #### ######### ##  #### #########
+##     ##  ##   ##      ##     ## ##   ##   ##     ##   ### ##     ## ##   ### ##     ##
+##     ## #### ########  #######  ##    ## ####    ##    ## ##     ## ##    ## ##     ##
+`
+)
+
+type Options struct {
+	Verbose bool
+}
+
+func NewOptions() *Options {
+	return &Options{}
+}
+
+func (o *Options) Run() error {
+	fmt.Printf("%s\n", banner)
+	return nil
+}
+
+func NewCmdMizukinana() *cobra.Command {
+	o := NewOptions()
+	cmd := &cobra.Command{
+		Use:   "mizukinana",
+		Short: "mizukinana is a Command-Line tool for providing some information of Mizuki Nana",
+		Run: func(cmd *cobra.Command, args []string) {
+			_ = o.Run()
+			_ = cmd.Usage()
+		},
+	}
+	cmd.PersistentFlags().BoolVarP(&o.Verbose, "verbose", "v", false, "verbose output")
+	cmd.AddCommand(version.NewCmdVersion())
+	cmd.AddCommand(nanaparty.NewCmdNanaparty())
+	return cmd
+}
+
+func main() {
+	cmd := NewCmdMizukinana()
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
